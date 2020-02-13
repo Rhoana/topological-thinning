4bc931316754f11c9da60c152e6a4f2df3a77531
bmatejek <bmatejek@gmail.com>
2020-02-13 18:57:58 -0500

    pyx: long to int64_t for windows

--- skeletonization/generate_skeletons.pyx
+++ skeletonization/generate_skeletons.pyx
@@ -8,6 +8,7 @@ cimport numpy as np
 from libcpp cimport bool
 import ctypes
 import numpy as np
+from libc.stdint cimport int64_t
 
 
 
@@ -17,9 +18,9 @@ from topological_thinning.utilities.constants import *
 
 
 cdef extern from 'cpp-generate_skeletons.h':
-    void CppTopologicalThinning(const char *prefix, long skeleton_resolution[3], const char *lookup_table_directory)
-    void CppFindEndpointVectors(const char *prefix, long skeleton_resolution[3], float output_resolution[3])
-    void CppApplyUpsampleOperation(const char *prefix, long *input_segmentation, long skeleton_resolution[3], float output_resolution[3])
+    void CppTopologicalThinning(const char *prefix, int64_t skeleton_resolution[3], const char *lookup_table_directory)
+    void CppFindEndpointVectors(const char *prefix, int64_t skeleton_resolution[3], float output_resolution[3])
+    void CppApplyUpsampleOperation(const char *prefix, int64_t *input_segmentation, int64_t skeleton_resolution[3], float output_resolution[3])
 
 
 
@@ -29,20 +30,20 @@ def TopologicalThinning(prefix, input_segmentation, skeleton_resolution=(80, 80,
     assert (input_segmentation.dtype == np.int64)
 
     start_time = time.time()
-    
+
     # convert the numpy arrays to c++
-    cdef np.ndarray[long, ndim=1, mode='c'] cpp_skeleton_resolution = np.ascontiguousarray(skeleton_resolution, dtype=ctypes.c_int64)
+    cdef np.ndarray[int64_t, ndim=1, mode='c'] cpp_skeleton_resolution = np.ascontiguousarray(skeleton_resolution, dtype=ctypes.c_int64)
     lut_directory = os.path.dirname(__file__)
 
     # call the topological skeleton algorithm
     CppTopologicalThinning(prefix.encode('utf-8'), &(cpp_skeleton_resolution[0]), lut_directory.encode('utf-8'))
-    
+
     # call the upsampling operation
-    cdef np.ndarray[long, ndim=3, mode='c'] cpp_input_segmentation = np.ascontiguousarray(input_segmentation, dtype=ctypes.c_int64)
+    cdef np.ndarray[int64_t, ndim=3, mode='c'] cpp_input_segmentation = np.ascontiguousarray(input_segmentation, dtype=ctypes.c_int64)
     cdef np.ndarray[float, ndim=1, mode='c'] cpp_output_resolution = np.ascontiguousarray(dataIO.Resolution(prefix), dtype=ctypes.c_float)
 
     CppApplyUpsampleOperation(prefix.encode('utf-8'), &(cpp_input_segmentation[0,0,0]), &(cpp_skeleton_resolution[0]), &(cpp_output_resolution[0]))
-        
+
     print ('Generated skeletons for {} in {:0.2f} seconds.'.format(prefix, time.time() - start_time))
 
 
@@ -52,7 +53,7 @@ def FindEndpointVectors(prefix, skeleton_resolution=(80, 80, 80)):
     start_time = time.time()
 
     # convert to numpy array for c++ call
-    cdef np.ndarray[long, ndim=1, mode='c'] cpp_skeleton_resolution = np.ascontiguousarray(skeleton_resolution, dtype=ctypes.c_int64)
+    cdef np.ndarray[int64_t, ndim=1, mode='c'] cpp_skeleton_resolution = np.ascontiguousarray(skeleton_resolution, dtype=ctypes.c_int64)
     cdef np.ndarray[float, ndim=1, mode='c'] cpp_output_resolution = np.ascontiguousarray(dataIO.Resolution(prefix), dtype=ctypes.c_float)
 
     CppFindEndpointVectors(prefix.encode('utf-8'), &(cpp_skeleton_resolution[0]), &(cpp_output_resolution[0]))
--- transforms/seg2seg.pyx
+++ transforms/seg2seg.pyx
@@ -1,5 +1,7 @@
 cimport cython
 cimport numpy as np
+from libc.stdint cimport int64_t
+
 
 
 import os
@@ -15,8 +17,8 @@ from topological_thinning.utilities import dataIO
 
 
 cdef extern from 'cpp-seg2seg.h':
-    void CppDownsampleMapping(const char *prefix, long *segmentation, float input_resolution[3], long output_resolution[3], long input_grid_size[3])
-    
+    void CppDownsampleMapping(const char *prefix, int64_t *segmentation, float input_resolution[3], int64_t output_resolution[3], int64_t input_grid_size[3])
+
 
 
 
@@ -30,10 +32,10 @@ def DownsampleMapping(prefix, segmentation, output_resolution=(80, 80, 80)):
     start_time = time.time()
 
     # convert numpy arrays to c++ format
-    cdef np.ndarray[long, ndim=3, mode='c'] cpp_segmentation = np.ascontiguousarray(segmentation, dtype=ctypes.c_int64)
+    cdef np.ndarray[int64_t, ndim=3, mode='c'] cpp_segmentation = np.ascontiguousarray(segmentation, dtype=ctypes.c_int64)
     cdef np.ndarray[float, ndim=1, mode='c'] cpp_input_resolution = np.ascontiguousarray(dataIO.Resolution(prefix), dtype=ctypes.c_float)
-    cdef np.ndarray[long, ndim=1, mode='c'] cpp_output_resolution = np.ascontiguousarray(output_resolution, dtype=ctypes.c_int64)
-    cdef np.ndarray[long, ndim=1, mode='c'] cpp_input_grid_size = np.ascontiguousarray(segmentation.shape, dtype=ctypes.c_int64)
+    cdef np.ndarray[int64_t, ndim=1, mode='c'] cpp_output_resolution = np.ascontiguousarray(output_resolution, dtype=ctypes.c_int64)
+    cdef np.ndarray[int64_t, ndim=1, mode='c'] cpp_input_grid_size = np.ascontiguousarray(segmentation.shape, dtype=ctypes.c_int64)
 
     # call c++ function
     CppDownsampleMapping(prefix.encode('utf-8'), &(cpp_segmentation[0,0,0]), &(cpp_input_resolution[0]), &(cpp_output_resolution[0]), &(cpp_input_grid_size[0]))
